91a1071e848a4f36bee21a631a1ff661d8af9cb2
1forh <1forhlol@gmail.com>
2017-09-17 15:06:09 -0400

    refactors API_URL and swaps to https

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -6,18 +6,20 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 
+const API_URL = 'https://pokeapi.co/api/v2';
+
 @Injectable()
 export class PokemonService {
 	constructor(private http: Http) {}
 
 	public getPokemonList(): Observable<any> {
-		return this.http.get('http://pokeapi.co/api/v2/pokemon')
+		return this.http.get(`${API_URL}/pokemon`)
 			.map((response: Response) => response.json())
 			.catch((error: any) => Observable.throw(error.json()));
 	}
 
 	public getPokemon(id: number): Observable<any> {
-		return this.http.get(`http://pokeapi.co/api/v2/pokemon/${id}`)
+		return this.http.get(`${API_URL}/pokemon/${id}`)
 			.map((response: Response) => response.json())
 			.catch((error: any) => Observable.throw(error.json()));
 	}
